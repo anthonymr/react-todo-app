cb389482f85e096f949144581e1eeb821e6a5e64
Anthony Martin <anthonymartin20@gmail.com>
2023-02-20 17:36:14 -0400

    Create bassic Wepack + React setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+import reactLogo from '../images/react.svg';
+
+function App() {
+  return (
+  <div>
+    <h1>Hello from React application</h1>
+    <img src={reactLogo} className="logo react" alt="React logo" />
+  </div>
+  );
+}
+
+export default App;
